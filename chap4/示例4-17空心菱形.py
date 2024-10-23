f30259ab74ee6c84729f1c76eab240e208c9460c
guanqing <guanqinghz@163.com>
2024-10-23 14:08:03 +0800

    空心棱形

--- chap4/示例4-17空心菱形.py
+++ chap4/示例4-17空心菱形.py
@@ -7,16 +7,22 @@ for i in range(1, top_row + 1):
     for k in range(1, top_row + 1 - i):
         print(' ', end='')
     for j in range(1, 2 * i):
-        print('*', end='')
+        if j == 1 or j == i * 2 - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
     print('')
 
 bottom_row = (row - 1) // 2
 for i in range(1, bottom_row + 1):
     # 直角三角形
-    for j in range(1, i+1):
+    for j in range(1, i + 1):
         print(' ', end='')
 
     # 倒三角
-    for k in range(1, 2 * bottom_row - 2*i + 2):
-        print('*', end='')
+    for k in range(1, 2 * bottom_row - 2 * i + 2):
+        if k == 1 or k == 2 * bottom_row - 2 * i + 2 - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
     print()
