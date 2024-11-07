9cfb2cfbfe0c3ca1af924881438b3985898f3253
guanqing <guanqinghz@163.com>
2024-11-07 09:33:09 +0800

    第十一章开始

---
+++ chap11/示例11-1文件的读写操作.py
@@ -0,0 +1,24 @@
+def my_write():
+    # (1)(创建)打开文件
+    file = open('a.txt', 'w', encoding='utf-8')
+    # (2)操作文件
+    file.write('伟大的中国梦')
+    # (3)关闭
+    file.close()
+
+
+# 读取
+def my_read():
+    # (1)(创建)打开文件
+    file = open('a.txt', 'r', encoding='utf-8')
+    # (2)操作文件
+    s = file.read()
+    print(type(s), s)
+    # (3)关闭
+    file.close()
+
+
+# 主程序运行
+if __name__ == '__main__':
+    # my_write()  # 调用函数
+    my_read()
