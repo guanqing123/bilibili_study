7256e35a0e79d68175676dc30b4a572185cd2072
guanqing <guanqinghz@163.com>
2025-07-24 09:12:33 +0800

    多人聊天室

--- chap14-多人聊天室/server.py
+++ chap14-多人聊天室/server.py
@@ -1,3 +1,4 @@
+import time
 from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 
@@ -38,7 +39,7 @@ class YsjServer(wx.Frame):
         '''服务器功能实现的必要性'''
         self.isOn = False  # 存储服务器的启动状态,默认值False,默认没有启动
         # 服务器端绑定的IP地址和端口
-        self.host_port = ('', 8888)  # 空的字符串表示本机IP地址
+        self.host_port = ('', 9090)  # 空的字符串表示本机IP地址
         # 创建socket对象
         self.server_socket = socket(AF_INET, SOCK_STREAM)
         # 绑定IP地址和端口
@@ -50,6 +51,31 @@ class YsjServer(wx.Frame):
 
         # 当鼠标点击"启动服务"按钮时,要执行的操作
         self.Bind(wx.EVT_BUTTON, self.start_server, start_server_btn)
+        # 为保存聊天记录按钮绑定事件
+        self.Bind(wx.EVT_BUTTON, self.save_record, record_btn)
+        # 为断开按钮绑定事件
+        self.Bind(wx.EVT_BUTTON, self.stop_server, stop_server_btn)
+
+    def stop_server(self, event):
+        if self.isOn:
+            # 停止服务
+            self.isOn = False
+            # 停止服务,关闭socket对象
+            self.server_socket.close()
+            # 输出提示信息
+            print('服务器已停止')
+            # 遍历字典,关闭会话线程
+            for session_thread in self.session_thread_dict.values():
+                session_thread.isOn = False
+                session_thread.client_socket.close()
+            # 删除字典中所有的元素
+            self.session_thread_dict.clear()
+
+    def save_record(self, event):
+        # 获取只读文本框中的内容
+        record_data = self.show_text.GetValue()
+        with open('record.log', 'w', encoding='utf-8') as f:
+            f.write(record_data)
 
     def start_server(self, event):
         # 判断服务器是否已经启动,只有服务器没有启动,才能启动服务器
@@ -62,6 +88,8 @@ class YsjServer(wx.Frame):
             main_thread.daemon = True
             # 启动主线程
             main_thread.start()
+            print('服务器启动成功')
+            # 需要把启动服务按钮变灰,禁止点击
 
     def do_work(self):
         # 判断isOn的值
@@ -76,8 +104,22 @@ class YsjServer(wx.Frame):
             self.session_thread_dict[user_name] = session_thread
             # 启动会话线程
             session_thread.start()
+            # 输出服务器的提示信息
+            self.show_info_and_send_client('服务器通知', f'欢迎{user_name}进入聊天室',
+                                           time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
         # 当self.isOn的值为False时, 执行关闭socket对象
         self.server_socket.close()
+
+    def show_info_and_send_client(self, data_source, data, datetime):
+        # 字符串操作
+        send_data = f'{data_source}:{data}\n时间:{datetime}'
+        # 只读文本框
+        self.show_text.AppendText('-' * 40 + '\n' + send_data + '\n')
+        # 每一个客户端都发送一次
+        for client in self.session_thread_dict.values():
+            # 如果当前的会话是否是开启状态
+            if client.isOn:
+                client.client_socket.send(send_data.encode('utf-8'))
 
 
 # 服务器端会话线程的类
@@ -98,9 +140,14 @@ class SessionThread(Thread):
             # 如果客户端点击断开按钮,先给服务器发送一句话, 消息自定义 Y-disconnect-SJ 自定义的结束词
             if data == 'Y-disconnect-SJ':
                 self.isOn = False
-            else:
+                # 发送一条服务器通知
+                self.server.show_info_and_send_client('服务器通知', f'{self.user_name}已经退出聊天室',
+                                                      time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+            elif data:
                 # 其他聊天信息显示给所有的客户端,包含服务器也显示
-                pass
+                # 调用刚才编写的方法
+                self.server.show_info_and_send_client(self.user_name, data,
+                                                      time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
         # 关闭socket
         self.client_socket.close()
 
